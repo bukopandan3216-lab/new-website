180dc22d2e695763b246afc07da5911771b8b228
bukopandan3216-lab <bukopandan3216@gmail.com>
2026-05-22 09:53:10 +0800

    Update multer.js

--- server/config/multer.js
+++ server/config/multer.js
@@ -3,8 +3,9 @@ const path = require('path')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
-  },
+  cb(null, 'uploads/products')
+},
+  
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`)
   },
