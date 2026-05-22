455b61566b231bc47931401d7f78cfa5b96662ec
bukopandan3216-lab <bukopandan3216@gmail.com>
2026-05-22 11:09:40 +0800

    Refactor database configuration to use environment variables

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'kodama.proxy.rlwy.net',
-  port: process.env.DB_PORT || 36565,
-  user: process.env.DB_USER || 'root',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'railway',
+  database: process.env.DB_NAME,
 
   waitForConnections: true,
   connectionLimit: 10,
