8dff53edd5e48d54b07bfce03cf1330f720f978d
bukopandan3216-lab <bukopandan3216@gmail.com>
2026-05-22 09:50:05 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://thefarmerww.vercel.app',
+  credentials: true
+}))
 app.use(express.json())
 
 app.get('/', (req, res) => {
