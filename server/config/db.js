6e4342ee7eefe43ec7bc6f2b79fb698c51fd38b8
bukopandan3216-lab <bukopandan3216@gmail.com>
2026-05-22 11:20:03 +0800

    Modify DB connection settings in db.js
    Updated database configuration to use hardcoded values for connection parameters.

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  //host: process.env.DB_HOST,
+ // port: process.env.DB_PORT,
+ // user: process.env.DB_USER,
+ // password: process.env.DB_PASSWORD,
+  //database: process.env.DB_NAME,
+  
+
+DB_HOST= mysql.railway.internal,
+DB_PORT=3306,
+DB_USER=root,
+DB_PASSWORD=your_password,
+DB_NAME=railway,
+MYSQL_DATABASE=railway,
 
   waitForConnections: true,
   connectionLimit: 10,
