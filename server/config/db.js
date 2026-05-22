5ad88a995b346e3bb2901c15cab15050d7035632
bukopandan3216-lab <bukopandan3216@gmail.com>
2026-05-22 11:02:31 +0800

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise')
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'kodama.proxy.rlwy.net',
+  port: process.env.DB_PORT || 36565,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'thefarmers',
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'railway',
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
